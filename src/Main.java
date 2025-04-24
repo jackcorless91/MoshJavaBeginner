2e07e509f31e50167cdf5b368f2905a263408d01
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-04-24 19:30:26 +1000

    primitive types

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ package com.jackcorless;
 
 public class Main {
     public static void main(String[] args) {
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
+        byte myAge = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+
+//              PRIMITIVE TYPES
+//        'byte', 1 byte = [-128, 127]
+//        'short', 3 bytes = [-32k, 32k]
+//        'int', 4 bytes = [-2b, 2b]
+//        'long' 8 bytes
+//        'float', 4 bytes
+//        'double', 8 bytes
+//        'char', 2 bytes ('')
+//        'boolean' 1 byte
     }
 }
