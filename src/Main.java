2dae40b3b6c2d85a16d03ed94b10322a4993139b
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-04-24 19:52:26 +1000

    strings

--- src/Main.java
+++ src/Main.java
@@ -22,11 +22,17 @@ public class Main {
 
 
         Date now = new Date();
-        System.out.println(now);
+//        System.out.println(now);
 
         Point point1 = new Point(1, 1);
         Point point2 = point1;
         point1.x = 2;
-        System.out.println(point2);
+//        System.out.println(point2);
+
+        String message = "yo" + "!!   ";
+        System.out.println(message.replace("!", "*"));
+        System.out.println(message);
+
+
     }
 }
